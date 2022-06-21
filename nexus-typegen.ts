8d92fc6040770a5fb37b0ab3c9beace4bde3faad
Gwangwoo Park <gwangwoo.park@gmail.com>
2022-06-22 06:39:02 +0900

    update database & add prisma client and context

--- nexus-typegen.ts
+++ nexus-typegen.ts
@@ -4,7 +4,7 @@
  */
 
 
-
+import type { Context } from "./src/context"
 
 
 
@@ -87,7 +87,7 @@ export type NexusGenFeaturesConfig = {
 }
 
 export interface NexusGenTypes {
-  context: any;
+  context: Context;
   inputTypes: NexusGenInputs;
   rootTypes: NexusGenRootTypes;
   inputTypeShapes: NexusGenInputs & NexusGenEnums & NexusGenScalars;
