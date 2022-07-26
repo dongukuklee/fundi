7e5c1a481334a830da9e17cb921e410fe6d7a142
dongukuklee <xehddnr@naver.com>
2022-07-26 09:41:26 +0900

    앱 푸시 함수 추가

--- src/utils/AppPushMessage.ts
+++ src/utils/AppPushMessage.ts
@@ -1,11 +1,48 @@
 import admin from "firebase-admin";
-import { DataMessagePayload } from "firebase-admin/lib/messaging/messaging-api";
+require("dotenv").config();
 
-const message = async (tokens: string[], data: DataMessagePayload) => {
-  await admin.messaging().sendToDevice(tokens, data, {
-    // Required for background/quit data-only messages on iOS
-    contentAvailable: true,
-    // Required for background/quit data-only messages on Android
-    priority: "high",
-  });
+type notification = {
+  title: string;
+  body: string;
+};
+
+type notificationData = {
+  notification: notification;
+  token: string;
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.APP_PUSH_PRIVATE_KEY,
+    projectId: process.env.PROJECT_ID,
+  }),
+});
+
+const message = (data: notificationData) => {
+  admin
+    .messaging()
+    .send(data)
+    .then(function (response) {
+      console.log("Successfully sent message: : ", response);
+    })
+    .catch(function (err) {
+      console.log("Error Sending message!!! : ", err);
+    });
+};
+
+export const sendMessageToDevice = async (
+  token: string,
+  title: string,
+  body: string
+) => {
+  const data = {
+    notification: {
+      title,
+      body,
+    },
+    token,
+  };
+
+  await message(data);
 };
