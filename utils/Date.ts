b0318923fa8cdb1a605085a5d67bf372bc08bf63
dongukuklee <xehddnr@naver.com>
2022-10-13 09:56:11 +0900

    YYYYMMDD 날짜 포맷 함수 추가

--- utils/Date.ts
+++ utils/Date.ts
@@ -3,3 +3,12 @@ export const getLocalDate = () => {
   date.setHours(date.getHours() + 9);
   return date;
 };
+
+export const getFormatDate = (date: Date) => {
+  let year = date.getFullYear();
+  let month: string | number = date.getMonth() + 1;
+  month = month >= 10 ? month.toString() : "0" + month;
+  let day: string | number = date.getDate();
+  day = day >= 10 ? day : "0" + day;
+  return year + month + day;
+};
